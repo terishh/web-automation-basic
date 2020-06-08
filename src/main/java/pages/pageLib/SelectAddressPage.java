e26de0d140cef28a4c1b157340e1d61b5e633f83
terishh <59705939+terishh@users.noreply.github.com>
2020-06-09 00:07:04 +0300

    Update SelectAddressPage.java

--- src/main/java/pages/pageLib/SelectAddressPage.java
+++ src/main/java/pages/pageLib/SelectAddressPage.java
@@ -1,7 +1,10 @@
 package pages.pageLib;
-
+import java.util.Map;
 import com.codeborne.selenide.SelenideElement;
+import io.cucumber.datatable.DataTable;
 import pages.PageManager;
+import java.lang.Boolean;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelectAddressPage extends BasePage {
   // Constructor
@@ -10,6 +13,8 @@ public class SelectAddressPage extends BasePage {
     addElement("radioButton", "mat-radio-button[class*=mat-radio-button]");
     addElement("continue", "button[aria-label='Proceed to payment selection']");
     addElement("timTester", "//mat-row[contains(., 'Tim Tester')]//mat-radio-button");
+    addElement("addNewAddress", "button[aria-label='Add a new address']");
+
   }
   // Methods
   @Override
@@ -20,7 +25,28 @@ public class SelectAddressPage extends BasePage {
   public void navigate(String element) {
     switch (element){
       case "continue": PageManager.setCurrentPage(PageManager.getDeliveryPage()); break;
+      case "addNewAddress": PageManager.setCurrentPage(PageManager.getCreateAddressPage()); break;
       default        : super.navigate(element);
+    }
+  }
+  public void doAction(String action, DataTable dataTable) {
+    switch(capitalizeSecond(action)){
+      case "validateAddress": validateAddress(dataTable); break;
+      default: super.doAction(action, dataTable);
+    }
+  }
+
+  public void validateAddress(DataTable dataTable) {
+    Map<String,String> data = dataTable.transpose().asMaps().get(0);
+    info("Validating address:\n" + data);
+    if(data.get("Name") != null){
+      assertThat(isTextVisible(data.get("Name"))).isTrue();
+    }
+    if(data.get("Address") != null){
+      assertThat(isTextVisible(data.get("Address"))).isTrue();
+    }
+    if(data.get("Country") != null){
+      assertThat(isTextVisible(data.get("Country"))).isTrue();
     }
   }
 }
