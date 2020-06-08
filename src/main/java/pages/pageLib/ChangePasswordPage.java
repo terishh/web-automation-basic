911557c573620bc8671fede26f0392ed4a17066e
terishh <59705939+terishh@users.noreply.github.com>
2020-06-09 00:09:19 +0300

    Update ChangePasswordPage.java

--- src/main/java/pages/pageLib/ChangePasswordPage.java
+++ src/main/java/pages/pageLib/ChangePasswordPage.java
@@ -1,14 +1,32 @@
 package pages.pageLib;
-
 import com.codeborne.selenide.SelenideElement;
+import pages.PageManager;
+
 
 public class ChangePasswordPage extends BasePage {
   //Constructor
   public ChangePasswordPage() {
     addElement("title", "//h1[contains(., 'Change Password')]");
+    addElement("currentPasswordField", "input[aria-label='Field to enter the current password']");
+    addElement("newPasswordField", "input[aria-label='Field for the new password']");
+    addElement("repeatNewPasswordField", "input[aria-label='Field to repeat the new password']");
+    addElement("change", "button[aria-label='Button to confirm the change']");
+    addElement("accountButton", "button[aria-label='Show/hide account menu']");
+    addElement("logout", "button[aria-label='Logout']");
+
   }
   @Override
   public SelenideElement getMainElement() {
     return getElement("title");
   }
+  @Override
+  public void navigate(String element) {
+    switch (element){
+      case "logout":
+        PageManager.setCurrentPage(PageManager.getHomePage()); break;
+    }
+  }
+  
+
+  
 }
